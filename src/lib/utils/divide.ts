503942c305d1ad718e695176e030a8425afef5df
Roman <dev@rman.dev>
2026-07-03 21:21:51 +0100

    chore: simplify divide

--- src/lib/utils/divide.ts
+++ src/lib/utils/divide.ts
@@ -5,65 +5,43 @@ import { ZERO } from "./constants";
 import { gcd } from "./gcd";
 import { simplify } from "./simplify";
 import { OverflowValue } from "./types";
-import type { Value, ValueConstant } from "./types";
+import type { Value, ValueExponent } from "./types";
 
 export function divide(left: Value, right: Value): Value {
   const lN = left.n;
   const rN = right.n;
 
   if (rN === 0n) throw new DivisionByZeroError();
-
   if (lN === 0n) return ZERO;
+  if (lN === "OVERFLOW" || rN === "OVERFLOW") return OverflowValue;
 
-  if (lN === "OVERFLOW" || rN === "OVERFLOW") {
-    return OverflowValue;
-  }
+  const { d: lD, c: lC, e: lE } = left;
+  const { d: rD, c: rC, e: rE } = right;
 
   const g1 = gcd(lN, rN);
-  const lD = left.d;
-  const rD = right.d;
   const g2 = gcd(rD, lD);
-
   const n = (lN / g1) * (rD / g2);
   const d = (lD / g2) * (rN / g1);
 
-  let c: ValueConstant | undefined;
-  let nExp: bigint | undefined;
-
-  // Handle constants
-  if (left.c !== undefined && right.c === undefined) {
-    c = left.c;
-  } else if (left.c === undefined && right.c !== undefined) {
-    c = right.c;
-  } else if (left.c === right.c) {
-    c = left.c;
-  }
-
-  // Handle exponents (Subtract right from left during division)
-  const lExpN = left.e?.n ?? (left.c !== undefined ? 1n : undefined);
-  const rExpN = right.e?.n ?? (right.c !== undefined ? 1n : undefined);
-
-  const lExpD = left.e?.d ?? 1n;
-  const rExpD = right.e?.d ?? 1n;
-
-  if (lExpN !== undefined && rExpN !== undefined && left.c === right.c) {
+  if (lC !== undefined && lC === rC) {
     const e = simplify(
-      add({ n: lExpN, d: lExpD }, { n: rExpN, d: rExpD }, true),
+      add(
+        { n: lE?.n ?? 1n, d: lE?.d ?? 1n },
+        { n: rE?.n ?? 1n, d: rE?.d ?? 1n },
+        true,
+      ),
     );
-    return { n, d, c, e };
+    return { n, d, c: lC, e };
   }
 
-  let dExp;
+  const c = lC ?? rC;
 
-  if (lExpN !== undefined) {
-    nExp = lExpN;
-    dExp = lExpD;
-  } else if (rExpN !== undefined) {
-    nExp = -rExpN; // Moving from denominator to numerator negates the exponent
-    dExp = rExpD;
+  let e: ValueExponent | undefined;
+  if (lC !== undefined) {
+    e = lE ? { n: lE.n, d: lE.d ?? 1n } : { n: 1n, d: 1n };
+  } else if (rC !== undefined) {
+    e = { n: -(rE?.n ?? 1n), d: rE?.d ?? 1n };
   }
-
-  const e = nExp ? { n: nExp, d: dExp } : undefined;
 
   return { n, d, c, e };
 }
